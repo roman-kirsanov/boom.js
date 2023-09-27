089e9d622c3b462923a8f3ad739c38ee01f29d2b
Roman Kirsanov <kirsanov2008@gmail.com>
2023-09-26 21:07:53 -0600

    Process API ✅

--- api/src/Process.cpp
+++ api/src/Process.cpp
@@ -3,62 +3,70 @@
 #include <iostream>
 #include <filesystem>
 #include <libgen.h>
+#include <Boom.hpp>
 #include <Boom/API/Process.hpp>
 
 namespace boom::api {
 
 void InitProcessAPI(boom::js::ContextRef context, char const** argv, size_t argc, char const** envp) {
     assert(context != nullptr);
+    assert(envp != nullptr);
+    assert(argv != nullptr);
+    assert(argc > 0);
 
-    auto process = boom::js::Value::Object(context, {});
-    auto arguments = std::vector<boom::js::ValueRef>();
-    auto envvars = std::map<std::string, boom::js::ValueRef>();
-    auto execpath = std::filesystem::path(argv[0]);
-    auto execdir = execpath.parent_path();
+    auto process = boom::js::Value::Object(context);
+    auto stdout_ = boom::js::Value::Object(context);
+    auto stderr_ = boom::js::Value::Object(context);
 
-    for (std::size_t i = 0; i < argc; i++) {
-        arguments.push_back(boom::js::Value::String(context, argv[i]));
-    }
-
-    for (auto env = envp; *env != 0; env++) {
-        auto entry = std::string(*env);
-        auto index = entry.find("=");
-        if (index != std::string::npos) {
-            auto name = entry.substr(0, index);
-            auto value = entry.substr(index + 1, entry.size() - (index + 1));
-            envvars.insert({ name, boom::js::Value::String(context, value) });
-        }
-    }
-
-    process->setProperty("argv", boom::js::Value::Array(context, arguments));
-    process->setProperty("env", boom::js::Value::Object(context, envvars));
-    process->setProperty("execDir", boom::js::Value::String(context, execdir));
-    process->setProperty("execPath", boom::js::Value::String(context, execpath));
-
-    process->defineProperty("workDir", boom::js::Value::Function(context, [](auto context, auto, auto) -> boom::js::Result {
-        return boom::js::Value::String(context, std::filesystem::current_path().string());
-    }));
-
-    process->setProperty("exit", boom::js::Value::Function(context, [](auto context, auto, auto arguments) -> boom::js::Result {
-        if (arguments.size() == 0) {
-            std::exit(0);
-        } else if (arguments.size() == 1) {
-            if (auto code = arguments[0]->asNumber()) {
-                std::exit(static_cast<int>(code.value()));
-            } else {
-                return std::unexpected(
-                    boom::js::Value::Error(context, "First argument must be a number")
-                );
+    stdout_->setProperty("write", boom::js::Value::Function(context, [](auto context, auto, auto arguments) {
+        for (auto value : arguments) {
+            if (value->isString()) {
+                auto string = value->stringValue().value();
+                std::fprintf(stdout, "%s", string.c_str());
+                std::fflush(stdout);
+            } else if (value->isUint8Array()) {
+                auto array = value->uint8ArrayValue().value();
+                std::fprintf(stdout, "%.*s", (int)array.size(), array.data());
+                std::fflush(stdout);
             }
-        } else {
-            return std::unexpected(
-                boom::js::Value::Error(context, "Wrong number of arguments")
-            );
         }
         return boom::js::Value::Undefined(context);
-    }));
+    }), { .readOnly = true });
 
-    context->globalThis()->setProperty("process", process);
+    stderr_->setProperty("write", boom::js::Value::Function(context, [](auto context, auto, auto arguments) {
+        for (auto value : arguments) {
+            if (value->isString()) {
+                auto string = value->stringValue().value();
+                std::fprintf(stderr, "%s", string.c_str());
+                std::fflush(stderr);
+            } else if (value->isUint8Array()) {
+                auto array = value->uint8ArrayValue().value();
+                std::fprintf(stderr, "%.*s", (int)array.size(), array.data());
+                std::fflush(stderr);
+            }
+        }
+        return boom::js::Value::Undefined(context);
+    }), { .readOnly = true });
+
+    process->setProperty("argv", boom::js::Value::Array(context, boom::ParseArgs(argv, argc)), { .readOnly = true });
+    process->setProperty("env", boom::js::Value::Object(context, boom::ParseEnvs(envp)), { .readOnly = true });
+    process->setProperty("execDir", boom::js::Value::String(context, std::filesystem::path(argv[0])), { .readOnly = true });
+    process->setProperty("execPath", boom::js::Value::String(context, std::filesystem::path(argv[0]).parent_path()), { .readOnly = true });
+    process->setProperty("exit", boom::js::Value::Function(context, [](auto context, auto, auto arguments) {
+        std::exit(
+            (arguments.size() > 0)
+                ? arguments[0]->numberValue().value_or(0)
+                : 0
+        );
+        return boom::js::Value::Undefined(context);
+    }), { .readOnly = true });
+    process->defineProperty("workDir", boom::js::Value::Function(context, [](auto context, auto, auto) {
+        return boom::js::Value::String(context, std::filesystem::current_path().string());
+    }));
+    process->setProperty("stdout", stdout_, { .readOnly = true });
+    process->setProperty("stderr", stderr_, { .readOnly = true });
+
+    context->globalThis()->setProperty("process", process, { .readOnly = true });
 }
 
 } /* namespace boom::api */
---
+++ api/src/Process.d.ts
@@ -0,0 +1,16 @@
+declare type Process = {
+    readonly argv: Readonly<string[]>;
+    readonly env: Readonly<Record<string, string>>;
+    readonly workDir: string;
+    readonly execDir: string;
+    readonly execPath: string;
+    readonly exit: (code?: number) => void;
+    readonly stdout: {
+        readonly write: (...args: (string | Uint8Array)[]) => void;
+    },
+    readonly stderr: {
+        readonly write: (...args: (string | Uint8Array)[]) => void;
+    }
+}
+
+declare const process: Readonly<Process>;
