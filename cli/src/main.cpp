5ac3679c132cf637942a16a9a754d93b46ef3b28
Roman Kirsanov <kirsanov2008@gmail.com>
2023-09-27 00:26:01 -0600

    Improved cmake config

--- cli/src/main.cpp
+++ cli/src/main.cpp
@@ -8,5 +8,30 @@ int main(int argc, char const *argv[], char const *envp[]) {
 
     boom::api::InitProcessAPI(context, argv, argc, envp);
 
+
+    auto function = boom::js::Value::Function(context, [](auto context, auto thisObject, auto arguments) {
+        thisObject->setProperty("_prop1", boom::js::Value::String(context, "value1"));
+        thisObject->setProperty("_prop2", boom::js::Value::String(context, "value2"));
+        return boom::js::Value::Undefined(context);
+    });
+
+    function->setProperty("prototype", boom::js::Value::Object(context, {
+        { "getProp1", boom::js::Value::Function(context, [](auto context, auto thisObject, auto arguments) { return thisObject->getProperty("_prop1"); }) },
+        { "TestProp1", boom::js::Value::String(context, "TestValue1") }
+    }));
+
+    context->globalThis()->setProperty("Test", function);
+
+    auto result = context->evaluate(
+        "process.stdout.write(JSON.stringify({ ...Test }, null, 4));\n"
+        "const v = new Test();\n"
+        "process.stdout.write(JSON.stringify(v, null, 4));\n"
+        "process.stdout.write(v.getProp1())"
+    );
+
+    if (result.has_value() == false) {
+        std::cerr << result.error()->toString().value() << std::endl;
+    }
+
     return 0;
 }
