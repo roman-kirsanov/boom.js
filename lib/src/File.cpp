c54b77085f7a8d62270d0405996c27a6880656f4
Roman Kirsanov <kirsanov2008@gmail.com>
2023-10-15 23:53:47 -0600

    trying to get windows working...

--- lib/src/File.cpp
+++ lib/src/File.cpp
@@ -2,35 +2,4 @@
 
 namespace boom {
 
-std::expected<std::vector<std::uint8_t>, std::string> FileRead(std::string const& path) {
-    auto reader = boom::FileReader(path);
-    if (reader) {
-        return reader.value()->readAll();
-    } else {
-        return std::unexpected(reader.error());
-    }
-}
-
-std::expected<void, std::string> FileWrite(std::string const& path, std::vector<std::uint8_t> const& data) {
-    auto writer = boom::FileWriter(path);
-    if (writer) {
-        writer.value()->write(data);
-        writer.value()->close();
-        return std::expected<void, std::string>();
-    } else {
-        return std::unexpected(writer.error());
-    }
-}
-
-std::expected<void, std::string> FileAppend(std::string const& path, std::vector<std::uint8_t> const& data) {
-    auto writer = boom::FileWriter(path, { .append = true });
-    if (writer) {
-        writer.value()->write(data);
-        writer.value()->close();
-        return std::expected<void, std::string>();
-    } else {
-        return std::unexpected(writer.error());
-    }
-}
-
 } /* namespace boom */
