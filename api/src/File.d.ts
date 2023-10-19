26f13dfe9440cfd63637e019309a619a3242545c
Roman Kirsanov <kirsanov2008@gmail.com>
2023-10-18 19:28:42 -0600

    working on File API

--- api/src/File.d.ts
+++ api/src/File.d.ts
@@ -3,11 +3,21 @@ declare type FileMode = {
     write?: boolean;
 }
 
+declare type FileSeek = {
+    Start,
+    Current,
+    End
+}
+
 declare class File {
     public constructor(path: string, mode?: FileMode);
     public text(): Promise<string>;
     public json(): Promise<any>;
     public bytes(): Promise<Uint8Array>;
+    public read(buffer: Uint8Array): Promise<number>;
+    public write(buffer: Uint8Array): Promise<void>;
+    public seek(mode: FileSeek): void;
+    public close(): void;
     public static Info(path: string): boolean;
     public static Exists(path: string): boolean;
     public static IsFile(path: string): boolean;
