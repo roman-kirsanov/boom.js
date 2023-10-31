698ba02b9e505bbc62ae285b967bc3974b6e7373
Roman Kirsanov <kirsanov2008@gmail.com>
2023-10-31 16:16:06 -0600

    re-engineering OpenGL generator

--- pkg/lib/include/Boom/OpenGL.hpp
+++ pkg/lib/include/Boom/OpenGL.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <optional>
 #include <Boom/Memory.hpp>
 
 namespace boom {
--- pkg/lib/task/opengl/index.js
+++ pkg/lib/task/opengl/index.js
@@ -1,139 +1,154 @@
-const { writeFileSync } = require('fs');
-const { defs, funcs } = require(__dirname + '/defs.json');
+const { readFileSync } = require('fs');
 
+/**
+ * @typedef {Object} TypeMap
+ * @property {string} glType
+ * @property {string} stdType
+ *
+ * @typedef {Object} Def
+ * @property {string} glName
+ * @property {string} boomName
+ * @property {string} value
+ *
+ * @typedef {Object} Arg
+ * @property {string} name
+ * @property {string} glType
+ * @property {string} stdType
+ *
+ * @typedef {Object} Func
+ * @property {string} name
+ * @property {Arg[]} args
+ * @property {string} returns
+ */
+
+/**
+ * @type {TypeMap[]}
+ */
 const TYPE_MAP = [
-    ['GLvoid', 'void'],
-    ['GLenum', 'std::uint32_t'],
-    ['GLboolean', 'std::uint8_t'],
-    ['GLbitfield', 'std::uint32_t'],
-    ['GLbyte', 'std::int32_t'],
-    ['GLubyte', 'std::uint8_t'],
-    ['GLshort', 'std::int16_t'],
-    ['GLushort', 'std::uint16_t'],
-    ['GLint', 'std::int32_t'],
-    ['GLuint', 'std::uint32_t'],
-    ['GLclampx', 'std::int32_t'],
-    ['GLsizei', 'std::int32_t'],
-    ['GLfloat', 'float'],
-    ['GLclampf', 'float'],
-    ['GLdouble', 'double'],
-    ['GLclampd', 'double'],
-    ['GLeglClientBufferEXT', 'void*'],
-    ['GLeglImageOES', 'void*'],
-    ['GLchar', 'char'],
-    ['GLcharARB', 'char'],
-    ['GLhalf', 'std::uint16_t'],
-    ['GLhalfARB', 'std::uint16_t'],
-    ['GLfixed', 'std::int32_t'],
-    ['GLintptr', 'std::intptr_t'],
-    ['GLintptrARB', 'std::intptr_t'],
-    ['GLsizeiptr', 'std::int64_t'],
-    ['GLsizeiptrARB', 'std::int64_t'],
-    ['GLint64', 'std::int64_t'],
-    ['GLint64EXT', 'std::int64_t'],
-    ['GLuint64', 'std::uint64_t'],
-    ['GLuint64EXT', 'std::uint64_t'],
-    ['GLsync', 'void*'],
-    ['GLDEBUGPROC', 'void*']
-];
+    { glType: 'GLvoid', stdType: 'void'},
+    { glType: 'GLenum', stdType: 'std::uint32_t'},
+    { glType: 'GLboolean', stdType: 'std::uint8_t'},
+    { glType: 'GLbitfield', stdType: 'std::uint32_t'},
+    { glType: 'GLbyte', stdType: 'std::int32_t'},
+    { glType: 'GLubyte', stdType: 'std::uint8_t'},
+    { glType: 'GLshort', stdType: 'std::int16_t'},
+    { glType: 'GLushort', stdType: 'std::uint16_t'},
+    { glType: 'GLint', stdType: 'std::int32_t'},
+    { glType: 'GLuint', stdType: 'std::uint32_t'},
+    { glType: 'GLclampx', stdType: 'std::int32_t'},
+    { glType: 'GLsizei', stdType: 'std::int32_t'},
+    { glType: 'GLfloat', stdType: 'float'},
+    { glType: 'GLclampf', stdType: 'float'},
+    { glType: 'GLdouble', stdType: 'double'},
+    { glType: 'GLclampd', stdType: 'double'},
+    { glType: 'GLeglClientBufferEXT', stdType: 'void*'},
+    { glType: 'GLeglImageOES', stdType: 'void*'},
+    { glType: 'GLchar', stdType: 'char'},
+    { glType: 'GLcharARB', stdType: 'char'},
+    { glType: 'GLhalf', stdType: 'std::uint16_t'},
+    { glType: 'GLhalfARB', stdType: 'std::uint16_t'},
+    { glType: 'GLfixed', stdType: 'std::int32_t'},
+    { glType: 'GLintptr', stdType: 'std::intptr_t'},
+    { glType: 'GLintptrARB', stdType: 'std::intptr_t'},
+    { glType: 'GLsizeiptr', stdType: 'std::int64_t'},
+    { glType: 'GLsizeiptrARB', stdType: 'std::int64_t'},
+    { glType: 'GLint64', stdType: 'std::int64_t'},
+    { glType: 'GLint64EXT', stdType: 'std::int64_t'},
+    { glType: 'GLuint64', stdType: 'std::uint64_t'},
+    { glType: 'GLuint64EXT', stdType: 'std::uint64_t'},
+    { glType: 'GLsync', stdType: 'void*'},
+    { glType: 'GLDEBUGPROC', stdType: 'void*'}
+]
 
-for (const [ from, to ] of [ ...TYPE_MAP ]) {
-    TYPE_MAP.push([ `${from}*`, `${to}*` ]);
-    TYPE_MAP.push([ `${from}**`, `${to}**` ]);
-    TYPE_MAP.push([ `${from} const*`, `${to} const*` ]);
-    TYPE_MAP.push([ `${from} const**`, `${to} const**` ]);
-    TYPE_MAP.push([ `${from} const* const*`, `${to} const* const*` ]);
+/**
+ * @param {string} message
+ * @returns {never}
+ */
+const abort = message => {
+    console.log('ERROR:', message);
+    process.exit(-1);
 }
 
-const retype = type => {
-    for (const [ from, to ] of TYPE_MAP) {
-        if (type === from) {
-            return to;
-        }
+/**
+ *
+ * @param {string} type
+ * @returns {string}
+ */
+const formatArgType = type => {
+    for (const { glType } of TYPE_MAP) {
+        if (type === `const ${glType} *`) return `${glType} const*`;
+        else if (type === `const ${glType} *`) return `${glType} const`;
+        else if (type === `const ${glType}`) return `${glType} const`;
+        else if (type === `${glType} *`) return `${glType}*`;
     }
     return type;
 }
 
-const formatName = name => (name[2].toLowerCase() + name.slice(3));
+/**
+ * @param {string} name
+ * @returns {string}
+ */
+const defNameToBoomName = name => {
+    return ('OpenGL' + (
+        name.toLowerCase()
+            .slice(3)
+            .split('_')
+            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+            .join('')
+            .replace('Rgba', 'RGBA')
+            .replace('Rgb', 'Rgb')
+            .replace('Bgra', 'BGRA')
+            .replace('Brg', 'BGR')
+            .replace('Lsb', 'LSB')
+            .replace('mask', 'Mask')
+    ))
+}
 
-const formatArgs = args => (
-    args.split(',')
-        .map(i => i.trim())
-        .map(i => i.replace(/\ \*/g, '* '))
-        .map(i => i.split(' '))
-        .map(parts => [ parts.slice(0, -1).join(' '), ...parts.slice(-1) ])
-        .map(([ type, name ]) => ([
-            retype(type),
-            name
-        ]))
-)
-
-if (process.argv.includes('--defs')) {
-    const convertName = value => {
-        let sliced = value.slice(3);
-        let result = '';
-        for (let i = 0; i < sliced.length; i++) {
-            if (i === 0) {
-                result += sliced[i].toUpperCase();
-            } else if (sliced[i] === '_') {
-                i += 1;
-                result += sliced[i].toUpperCase();
-            } else {
-                result += sliced[i].toLowerCase();
+/**
+ * @param {string} content
+ * @returns {Def[]}
+ */
+const extractDefs = content => {
+    const REGEXP = new RegExp(/#define\s+GL_([^\s]+)\s+(0x[0-9a-fA-F]+)/);
+    return (content.match(new RegExp(REGEXP, 'g')) ?? []).map(match => {
+        const [ , name, value ] = match.match(REGEXP);
+        if (name && value) {
+            return {
+                glName: name,
+                boomName: defNameToBoomName(name),
+                value
             }
+        } else {
+            abort(`Failed to extract definition info from "${match}"`);
         }
-        return ('OpenGL' + result);
-    }
-
-    for (const def of defs) {
-        console.log(`auto constexpr ${convertName(def.name)} = ${def.value};`);
-    }
+    })
 }
 
-if (process.argv.includes('--check-types')) {
-    const types = [];
-    for (const func of funcs) {
-        const args = formatArgs(func.args);
-        for (const [ type ] of args) {
-            if (!types.includes(retype(type))) {
-                types.push(retype(type));
+/**
+ * @param {string} content
+ * @returns {Func[]}
+ */
+const extractFuncs = content => {
+    const REGEXP = new RegExp(/typedef\s+([^\s]+)\s+\(APIENTRY.*\)\((.*)\);\s*\nGLAPI\s*.*\s*glad_(.*);/);
+    return (content.match(new RegExp(REGEXP, 'g')) ?? []).map(match => {
+        const [ , returns, args, name ] = match.match(REGEXP);
+        if (returns && args && name) {
+            return {
+                name,
+                args: args.split(',').map(i => i.trim()),
+                returns
             }
+        } else {
+            abort(`Failed to extract function info from "${match}"`);
         }
-    }
-    console.log(types);
+    })
 }
 
-if (process.argv.includes('--check-names')) {
-    const errors = [];
-    for (const func of funcs) {
-        const args = formatArgs(func.args);
-        for (const [ , name ] of args) {
-            if (name.includes('*')) {
-                if (!errors.includes(name)) {
-                    errors.push(name);
-                }
-            }
-        }
-    }
-    console.log(errors);
-}
+// Main...
+(() => {
+    const content = readFileSync(__dirname + '/glad.h').toString();
+    const funcs = extractFuncs(content);
+    const defs = extractDefs(content);
+    console.log(funcs);
+})()
 
-if (process.argv.includes('--methods-def')) {
-    for (const func of funcs) {
-        const name = formatName(func.name)
-        const args = formatArgs(func.args);
-        console.log(`void ${name}(${args.map(([ type, name ]) => `${type} ${name}`).join(', ')});`);
-    }
-}
-
-if (process.argv.includes('--methods-impl')) {
-    for (const func of funcs) {
-        const name = formatName(func.name)
-        const args = formatArgs(func.args);
-        console.log(`void OpenGL::${name}(${args.map(([ type, name ]) => `${type} ${name}`).join(', ')}) {`);
-        console.log(`    _current();`);
-        console.log(`    boom::${func.name}(${args.map(([ , name ]) => name).join(', ')});`);
-        console.log(`}`);
-        console.log('');
-    }
-}
