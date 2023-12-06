6603a4e36b311430307364520257cdfe9eda801f
Roman Kirsanov <kirsanov2008@gmail.com>
2023-12-05 20:32:35 -0600

    project restructure (2)

--- task/build/index.js
+++ task/build/index.js
@@ -8,31 +8,22 @@ if (process.argv.includes('--clean')) {
 
 const release = process.argv.includes('--release');
 const build = (release ? 'Release' : 'Debug');
+const path = (__dirname + '/../..');
+const out = (__dirname + '/../../.build/' + build);
+const dts = [];
 
-// Build lib...
-(() => {
-    const path = (__dirname + '/../../lib');
-    const out = (__dirname + '/../../lib/.build/' + build);
-    const dts = [];
+mkdirSync(out, { recursive: true });
+execSync(`cmake -DCMAKE_BUILD_TYPE=${build} ${path}`, { cwd: out, stdio: 'inherit' });
+if (platform() == 'win32') {
+    execSync(`msbuild boom.sln /p:Configuration=${build}`, { cwd: out, stdio: 'inherit' });
+} else {
+    execSync('make', { cwd: out, stdio: 'inherit' });
+}
 
-    mkdirSync(out, { recursive: true });
-    execSync(`cmake -DCMAKE_BUILD_TYPE=${build} ${path}`, { cwd: out, stdio: 'inherit' });
-    if (platform() == 'win32') {
-        execSync(`msbuild boom.sln /p:Configuration=${build}`, { cwd: out, stdio: 'inherit' });
-    } else {
-        execSync('make', { cwd: out, stdio: 'inherit' });
+for (const filename of readdirSync(path + '/src/API')) {
+    if (filename.endsWith('.d.ts')) {
+        dts.push(readFileSync(path + '/src/API/' + filename).toString());
     }
+}
 
-    for (const filename of readdirSync(path + '/src/API')) {
-        if (filename.endsWith('.d.ts')) {
-            dts.push(readFileSync(path + '/src/API/' + filename).toString());
-        }
-    }
-
-    writeFileSync(out + '/boom.d.ts', dts.join('\n\n'));
-})();
-
-// Build CLI...
-(() => {
-    execSync('pnpm i', { cwd: (__dirname + '/../../'), stdio: 'inherit' });
-})();
+writeFileSync(out + '/boom.d.ts', dts.join('\n\n'));
--- task/clean/index.js
+++ task/clean/index.js
@@ -1,4 +1,4 @@
 const { rmSync } = require('fs');
 
-rmSync(__dirname + '/../../lib/.build', { recursive: true, force: true });
+rmSync(__dirname + '/../../.build', { recursive: true, force: true });
 rmSync(__dirname + '/../../doc/example/TestCppApp/.build', { recursive: true, force: true });
